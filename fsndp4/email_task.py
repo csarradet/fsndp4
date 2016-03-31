9552580a2affe9f4f0a6d2874bc9642253cd9257
Cody Sarradet <csarradet@gmail.com>
2016-03-31 13:25:46 -0500

    feat: Implemented mail task

--- fsndp4/email_task.py
+++ fsndp4/email_task.py
@@ -1,24 +1,3 @@
-import logging
-from google.appengine.api import mail
-from google.appengine.ext import deferred
-
-
-# class SendReminderEmail(webapp2.RequestHandler):
-#     def get(self):
-#         """Send a reminder email to each User with an email about games.
-#         Called every hour using a cron job"""
-#         app_id = app_identity.get_application_id()
-#         users = User.query(User.email != None)
-#         for user in users:
-#             subject = 'This is a reminder!'
-#             body = 'Hello {}, try out Guess A Number!'.format(user.email)
-#             # This will send test emails, the arguments to send_mail are:
-#             # from, to, subject, body
-#             mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-#                            user.email,
-#                            subject,
-#                            body)
-
 """
 Email alerting system for users that have pending moves on old games
 (>24 hours since last update).
@@ -26,9 +5,48 @@ Email alerting system for users that have pending moves on old games
 Uses a task queue since the query/email process may take awhile; other
 modules should call start() to kick off a job.
 """
+import datetime
+import logging
+
+from google.appengine.api import mail
+from google.appengine.ext import deferred
+
+from models import Game
+
 
 def start():
+    logging.info("Firing email task")
     deferred.defer(__email_task)
 
 def __email_task():
-    logging.info("__email_task() fired")
+    logging.info("Email task started")
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    stale_game_q = Game.query(Game.active == True, Game.updated < yesterday)
+    stale_games = {}
+    for game in stale_game_q.iter():
+        player_key = game.active_player_key
+        if not player_key in stale_games:
+            stale_games[player_key] = []
+        stale_games[player_key].append(game.key.id())
+    __send_emails(stale_games)
+    logging.info("Email task complete")
+
+def __send_emails(stale_games_dict):
+    """ 
+    Moving this into its own function gives us the flexibility
+    to change the implementation later, such as by spawning 
+    additional worker threads if the task takes too long.
+    """
+    for i in stale_games_dict:
+        __send_email(player_key, game_list)
+
+def __send_email(player_key, game_list):
+    app_id = app_identity.get_application_id()
+    player = User.
+    subject = "Pending Liar's Dice games"
+    body = "You have the following pending games:\n\n{}".format(
+        '\n'.join(game_list))
+    mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+        User.email_from_key(player_key),
+        subject,
+        body)
